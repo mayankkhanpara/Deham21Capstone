652ad10d943715eaed9396739db611d471af5997
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-04-29 22:28:02 +0200

    Delete VPC File

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "aws_capstone_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "TadkaTwistVPC"
-  }
-}
