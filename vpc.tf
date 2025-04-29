25631eac2aafc91fdaa1b22f087252e526af3bd5
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-04-29 21:50:15 +0200

    Renamed file and added Terraform script for VPC creation

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,8 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version= "5.94.1"
-        }
-    }
-}
-
-# Creating VPC
-resource "aws_vpc" "wordpress-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "aws_capstone_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "AwsCapstoneVPC"
+  }
 }
