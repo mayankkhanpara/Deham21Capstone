784403fcf50e0b1b34eb9065a1f24b10c800cd01
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-05-09 17:31:58 +0200

    Remove Old infrastucture

--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "tadkatwist_images" {
-  bucket = "tadkatwist-images-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "Tadka Twist Image Storage"
-    Environment = "prod"
-  }
-}
