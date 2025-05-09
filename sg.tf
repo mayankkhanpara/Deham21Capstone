c2ca232b27df229f5e6aa9a6da2e22176b55e79b
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-05-09 18:22:45 +0200

    Fixed SG Issue

--- sg.tf
+++ sg.tf
@@ -27,12 +27,12 @@ resource "aws_security_group" "bastion_sg" {
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.tadka_vpc.id
 
-  # Allow SSH access only from the Bastion host
+  # Allow SSH access only from the Bastion host (security group reference)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.bastion_sg.ingress[0].cidr_blocks[0]]
+    security_groups = [aws_security_group.bastion_sg.id] # Use security group ID
   }
 
   # Allow all outbound traffic
@@ -45,6 +45,39 @@ resource "aws_security_group" "ec2_sg" {
 
   tags = {
     Name    = "tadka-ec2-sg"
+    Project = "Tadka Twist"
+  }
+}
+
+
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.tadka_vpc.id
+
+  # Allow HTTP and HTTPS traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "tadka-alb-sg"
     Project = "Tadka Twist"
   }
 }
