a887cd420c7e102b1d17260bd5ef2db1d16e12a6
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-05-09 21:38:39 +0200

    Added VPC resource for Tadka Twist infrastructure

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,8 @@
-provider "aws" {
-  region = "eu-central-1" # Frankfurt
-}
-
 resource "aws_vpc" "tadka_twist_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "tadka-twist-vpc"
   }
