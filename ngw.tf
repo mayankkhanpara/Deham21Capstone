df7cca3f3ee712d536dd83a11dacff39280d4338
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-05-10 10:54:10 +0200

    Added NAT Gateway and connected it to private route table

--- ngw.tf
+++ ngw.tf
@@ -1,4 +1,4 @@
-# 1. Allocate Elastic IP
+# 1. Allocate Elastic IP for NAT Gateway
 resource "aws_eip" "nat_eip" {
   vpc = true
   tags = {
@@ -6,38 +6,19 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
-# 2. Create NAT Gateway in Public Subnet
+# 2. NAT Gateway in Public Subnet
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_1.id
+  subnet_id     = aws_subnet.public_subnet.id
 
   tags = {
     Name = "tadka-twist-nat-gw"
   }
 }
 
-# 3. Route Table for Private Subnet
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.tadka_twist_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
-  tags = {
-    Name = "tadka-twist-private-rt"
-  }
-}
-
-# 4. Associate Route Table to Private Subnet A
-resource "aws_route_table_association" "private_subnet_1_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-# 5. Associate Route Table to Private Subnet B (if any)
-resource "aws_route_table_association" "private_subnet_2_association" {
-  subnet_id      = aws_subnet.private_subnet_b.id
-  route_table_id = aws_route_table.private_rt.id
+# 3. Add NAT route to existing private_rt
+resource "aws_route" "private_rt_nat_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
 }
