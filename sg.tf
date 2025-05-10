0a79492ef57adc146cb5ee528ad89b66fb8db28d
Mayankkumar Khanpara <pmayank355@gmail.com>
2025-05-10 10:36:16 +0200

    Added bastion, app, alb, and RDS security groups with proper SG-to-SG rules

--- sg.tf
+++ sg.tf
@@ -23,12 +23,13 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-# Security Group for Private EC2 App
+# Security Group for Application EC2 (private subnet)
 resource "aws_security_group" "app_sg" {
   name        = "app-sg"
   description = "Allow HTTP from ALB and SSH from Bastion"
   vpc_id      = aws_vpc.tadka_twist_vpc.id
 
+  # Allow SSH from Bastion Host SG
   ingress {
     from_port       = 22
     to_port         = 22
@@ -36,11 +37,12 @@ resource "aws_security_group" "app_sg" {
     security_groups = [aws_security_group.bastion_sg.id]
   }
 
+  # Allow HTTP from ALB SG
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Temporary until ALB is defined
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
@@ -52,6 +54,31 @@ resource "aws_security_group" "app_sg" {
 
   tags = {
     Name = "tadka-twist-app-sg"
+  }
+}
+
+# Security Group for Application Load Balancer
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP from Internet to ALB"
+  vpc_id      = aws_vpc.tadka_twist_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tadka-twist-alb-sg"
   }
 }
 
